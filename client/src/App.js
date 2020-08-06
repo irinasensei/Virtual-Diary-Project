7dc30b179c66a83fee404f487a48e49175ce0518
julieta <jumartinnob@gmail.com>
2020-08-06 09:43:23 +0200

    finished mysql and backend

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      Hello
-      </header>
+      <h2>MY VIRTUAL DIARY PROJECT</h2>
+      <textarea className="">text</textarea>
+      <input className="" placeholder="input text"></input>
+      <input className="" placeholder="input date"></input>
+      <button className="">Add this day</button>
     </div>
   );
 }
--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 router.use(bodyParser.json());
 
 /* GET diaries page. */
-router.get('/Diary', function(req, res, next) {
+router.get("/", function(req, res, next) {
     db("SELECT * FROM information")
     .then(results => {
         res.send(results.data);
@@ -14,12 +14,39 @@ router.get('/Diary', function(req, res, next) {
     .catch(err => res.status(500).send(err));
 });
 
-router.post("/Diary", (req, res) => {
-    db(`insert into information (date, text) values ("${req.body.date}", ${req.body.text});`)
+
+/* GET date page. Search for one specific date */
+router.get("/:date", function(req, res, next) {
+    const { date } = req.params;
+    if(date){
+    db("SELECT * FROM information WHERE date >= '2020-01-01' AND date <= '2021-01-01'")
+    // `SELECT * FROM information WHERE date = ${date}`
+    .then(results => {
+        res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+}
+});
+
+
+/* POST a new diaries. */
+router.post("/", (req, res) => {
+    db(`insert into information (date, text) values (STR_TO_DATE("${req.body.date}", '%d/%m/%Y'), "${req.body.text}");`)
     .then(() => {
         db("SELECT * FROM information;").then(result => res.send(result.data));
       })
     .catch(err => res.status(500).send(err));
   });
 
+
+/* DELETE one diaries page. */
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    if (id) {
+      db(`delete from information where id = "${id}"`)
+        .then(results => res.send("Item deleted"))
+        .catch(err => res.status(500).send(err));
+    }
+  });
+   
 module.exports = router;
