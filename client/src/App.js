a13bb7e615b0f92687cfbd2feae206fe0a5e9177
Irina <irinasensei@gmail.com>
2020-08-14 22:14:03 +0900

    front end edited

--- client/src/App.js
+++ client/src/App.js
@@ -1,158 +1,197 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       showError: false,
       text: "",
       date: "",
-      information: []
+      information: [],
     };
   }
 
-  //show my list in the div in the top 
+  //show my list in the div in the top
   componentDidMount() {
     this.getInformation();
   }
 
   getInformation = () => {
     fetch(`/diary`)
-      .then(response => response.json())
-      .then(response => {
-        this.setState({ 
-          information: response.map(each => { each.date = new Date(each.date).toDateString(); return each })
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({
+          information: response.map((each) => {
+            each.date = new Date(each.date).toDateString();
+            return each;
+          }),
         });
       });
   };
-  
 
   handleInput = (event) => {
     this.setState({
       text: event.target.value,
-      showError: false
+      showError: false,
     });
-  }
+  };
 
-  
   dateAgost = (event) => {
     this.setState({
-      date: event.target.value
+      date: event.target.value,
     });
-  }
+  };
 
-  addDay(){
-    
-    if(this.state.text === "" || this.state.text === undefined || this.state.date === "" || this.state.date === undefined) {
-      this.setState({ showError: true})
-    }
-    else {
-      this.setState({ showError: false})
+  addDay() {
+    if (
+      this.state.text === "" ||
+      this.state.text === undefined ||
+      this.state.date === "" ||
+      this.state.date === undefined
+    ) {
+      this.setState({ showError: true });
+    } else {
+      this.setState({ showError: false });
 
       fetch("/diary", {
         method: "POST",
-        headers: {"Content-Type": "application/json" }, 
-        body: JSON.stringify({ text: this.state.text, date: this.state.date })
-      }) .then(response => {
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ text: this.state.text, date: this.state.date }),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          } else {
+            return response.json();
+          }
+        })
+        .then((myJSON) => {
+          this.setState({ information: myJSON });
+          this.setState({
+            date: "",
+            text: "",
+          });
+        });
+    }
+  }
+
+  viewText(value) {
+    this.setState({ text: value });
+  }
+
+  deleteDate(id) {
+    fetch(`/diary/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         } else {
           return response.json();
         }
       })
-      .then(myJSON => {
-        this.setState({information: myJSON});
-         this.setState({
-            date: "",
-            text: ""
+      .then((myJSON) => {
+        this.setState({ information: myJSON });
+        this.setState({
+          date: "",
+          text: "",
         });
-      })
-    }
-  };
-
-  viewText(value) {
-    this.setState({text: value})
+      });
   }
 
-  deleteDate(id){
-    fetch(`/diary/${id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json"
-      },
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      } else {
-        return response.json();
-      }
-    })
-    .then(myJSON => {
-      this.setState({information: myJSON});
-      this.setState({
-        date: "",
-        text: ""
-      });
-    })
-    
-  };
+  function() {
+    `#datetimepicker12`.datetimepicker({
+      inline: true,
+      sideBySide: true,
+    });
+  }
 
-
- render () {
-   const { text, date, information } = this.state;
-  return (
-    <div className="App">
-
-        <h2>MY VIRTUAL DIARY PROJECT</h2>
-       
-        <div>{text}</div> <br></br><br></br>
-       <div>
-        <input 
-        type="text" 
-        value={text} 
-        onChange={this.handleInput} 
-        placeholder="How was your day?.."
-        maxLength="500"
-        /> <br></br><br></br>
-    
-        <div style={{ fontSize: 10, color: "red"}}>
-          {this.state.textError}          
+  render() {
+    const { text, date, information } = this.state;
+    return (
+      <div className="App">
+        <div className="container">
+          <h2>MY VIRTUAL DIARY PROJECT</h2>
+          <div>{text}</div> <br></br>
+          <br></br>
+          <div>
+            <textarea
+              type="text"
+              value={text}
+              onChange={this.handleInput}
+              placeholder="How was your day?.."
+              maxLength="500"
+              className="form-control"
+            ></textarea>{" "}
+            <br></br>
+            <br></br>
+            <div style={{ fontSize: 10, color: "red" }}>
+              {this.state.textError}
+            </div>
+          </div>
+          <input
+            type="date"
+            value={date}
+            onChange={this.dateAgost}
+            //placeholder="00/00/0000" //removed by Irina
+          />{" "}
+          <br></br>
+          <br></br>
+          {this.state.showError ? (
+            <div className="text-danger">
+              <i>All inputs are required</i>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          <br></br>
+          <button
+            onClick={(e) => this.addDay()}
+            className="btn btn-outline-secondary btn-sm "
+          >
+            <b>SAVE IN DATABASE</b>
+          </button>
+          <br></br>
         </div>
-
-        <input 
-        type="text" 
-        value={date} 
-        onChange={this.dateAgost} 
-        placeholder="00/00/0000" 
-        /> <br></br><br></br>
-        
-        {this.state.showError ? (<div className="text-danger"><i>All inputs are required</i></div>) : (<div></div>)}<br></br>
-        <button onClick={e => this.addDay()} className="btn btn-outline-secondary btn-sm "><b>SAVE IN DATABASE</b></button><br></br>
-        
-       </div>
-
-      <div className="col-3">
-            {information.map(each => {
-              return (
-                <div key={each.id} >
-                  <button onClick={() => this.viewText(each.text)} className="btn btn-outline-secondary btn-sm float-left ">
-                    {each.date}
-                  </button>
-                  <button onClick={() => this.deleteDate(each.id)} className="btn">delete
-                  </button>
+        <div className="container" id="pastrecords">
+          <p> My past records</p>
+          <div style="overflow:hidden;">
+            <div class="form-group">
+              <div class="row">
+                <div class="col-md-8">
+                  <div id="datetimepicker12"> </div>
                 </div>
-              );
-            })}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          {information.map((each) => {
+            return (
+              <div key={each.id}>
+                <button
+                  onClick={() => this.viewText(each.text)}
+                  className="btn btn-outline-secondary btn-sm float-left "
+                >
+                  {each.date}
+                </button>
+                <button
+                  onClick={() => this.deleteDate(each.id)}
+                  className="btn"
+                >
+                  delete
+                </button>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      </div>
-       
-
-  );
- };
-};
-
+    );
+  }
+}
 
 export default App;
--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require("express");
 const db = require("../model/helper");
 var router = express.Router();
 const bodyParser = require("body-parser");
@@ -6,39 +6,56 @@ const bodyParser = require("body-parser");
 router.use(bodyParser.json());
 
 /* GET diaries page. */
-router.get("/", function(req, res, next) {
-    db("SELECT * FROM information;")
-    .then(results => {
-        res.send(results.data);
+router.get("/", function (req, res, next) {
+  db("SELECT * FROM information;")
+    .then((results) => {
+      res.send(results.data);
+      console.log(results.data);
     })
-    .catch(err => res.status(500).send(err));
+    .catch((err) => res.status(500).send(err));
+});
+
+/* GET a specific page from diary */
+router.get("/:id", function (req, res, next) {
+  const { id } = req.params;
+  if (id) {
+    db(`SELECT * from information where id = "${id}";`)
+      .then((results) => {
+        res.status(200).send(results.data);
+      })
+      .catch((err) => res.status(500).send(err));
+  }
 });
 
 /* POST a new diaries. */
 router.post("/", (req, res) => {
-    db(`insert into information (date, text) values (STR_TO_DATE("${req.body.date}", '%d/%m/%Y'), "${req.body.text}");`)
+  // Julieta's code db(`insert into information (date, text) values (STR_TO_DATE("${req.body.date}", '%d/%m/%Y'), "${req.body.text}");`)
+  //below Irina's after change to date input
+  // 1st try db(insert into information (date,text) values ("${req.body.date}", "${req.body.text}");`)
+  //2nd try
+  db(
+    `insert into information (date, text) values ("${req.body.date}","${req.body.text}");`
+  )
     .then(() => {
-        db("SELECT * FROM information;").then(result => res.send(result.data));
-      })
-    .catch(err => res.status(500).send(err));
-  });
-
+      db("SELECT * FROM information;").then((result) => res.send(result.data));
+    })
+    .catch((err) => res.status(500).send(err));
+});
 
 /* DELETE one diaries page. */
 router.delete("/:id", (req, res) => {
-    const { id } = req.params;
-    if (id) {
-      
-      db(`delete from information where id = "${id}";`)
-        .then(() => {
-      db("SELECT * FROM information;")
-        .then(results => {
-          res.status(200).send(results.data);
-        })
-        .catch(err => res.status(500).send(err));
-    })
-    .catch(err => res.status(500).send(err));
-    }
-  });
-   
+  const { id } = req.params;
+  if (id) {
+    db(`delete from information where id = "${id}";`)
+      .then(() => {
+        db("SELECT * FROM information;")
+          .then((results) => {
+            res.status(200).send(results.data);
+          })
+          .catch((err) => res.status(500).send(err));
+      })
+      .catch((err) => res.status(500).send(err));
+  }
+});
+
 module.exports = router;
